f3d8829f7db7c471ef17f7474322990386eb3f71
dasooni <dr.amit@live.se>
2023-09-05 19:39:17 +0200

    Update interpolationmethods.h

--- modules/tnm067lab1/utils/interpolationmethods.h
+++ modules/tnm067lab1/utils/interpolationmethods.h
@@ -73,9 +73,10 @@ T bilinear(const std::array<T, 4> &v, F x, F y) {
 template <typename T, typename F = double>
 T quadratic(const T& a, const T& b, const T& c, F x) {
 
-    return (1 - x) * (1 - 2 * x) * a 
-        + 4 * x * (1 - x) * b 
-        + x * (2 * x - 1) * c;
+
+    return (1 - x) * (1 - 2 * x) * a  // (1-x)(1-2x)a
+        + 4 * x * (1 - x) * b 	   // 4x(1-x)b  
+        + x * (2 * x - 1) * c; 	   // x(2x-1)c
 }
 
 // clang-format off
@@ -96,11 +97,11 @@ T quadratic(const T& a, const T& b, const T& c, F x) {
 template <typename T, typename F = double>
 T biQuadratic(const std::array<T, 9>& v, F x, F y) {
 
-
+    // interpolate in x direction
     auto interPolatedVal1 = quadratic(v[0], v[1], v[2], x);
     auto interPolatedVal2 = quadratic(v[3], v[4], v[5], x);
     auto interPolatedVal3 = quadratic(v[6], v[7], v[8], x);
-
+    // interpolate in y direction and return
     return quadratic(interPolatedVal1, interPolatedVal2, interPolatedVal3, y);
 }
 
@@ -126,21 +127,23 @@ T barycentric(const std::array<T, 4>& v, F x, F y) {
     auto fB = v[1];
     auto fC = v[2];
 
+    // check if point is in the upper or lower triangle
     if (x + y < 1.0f) {
         alpha = 1.0f - x - y;
         beta = x;
         gamma = y;
-
+        // set the first point to the upper left corner
         fA = v[0];
+    // point is in the lower triangle
     } else {
         alpha = x + y - 1;
         beta = 1 - y;
         gamma = 1 - x; 
-
+        // set the first point to the lower left corner
         fA = v[3];
     }
     
-
+    // interpolate between the colors a and b
     return alpha * fA + beta * fB + gamma * fC;
 }
 
